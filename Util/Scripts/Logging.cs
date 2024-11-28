c7ae724db07dd74c164e0f0b8539dc24a15af83b
yoda-asobism <h-yoda@asobism.co.jp>
2024-11-28 09:48:23 +0900

    first commit

---
+++ Util/Scripts/Logging.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Snake.Gara.Unity.Basic.Library
+{
+    public class Logging : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
